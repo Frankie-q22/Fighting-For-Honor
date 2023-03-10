4c2d1d1714f5d84a8cd483c4fabec02a72ec9538
Frankie-q22 <124313378+Frankie-q22@users.noreply.github.com>
2023-03-10 10:48:28 -0700

    Add files via upload

---
+++ FightingForHonor.py
@@ -0,0 +1,69 @@
+import pygame
+from fighter import Fighter
+pygame.init()
+
+#creates Game Window
+Screen_Width = 1000
+Screen_Height = 600
+
+Screen = pygame.display.set_mode((Screen_Width,Screen_Height))
+pygame.display.set_caption("Fighting For Honor")
+
+
+#Set Framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#Define colors
+Red = (255,0,0)
+Yellow = (255,255,0)
+White = (255,255,255)
+
+#load BackGround Image
+background = pygame.image.load("GateJapan.jpg").convert_alpha()
+#Function that displays background
+def display_Bg():
+ scaledBG = pygame.transform.scale(background,(Screen_Width, Screen_Height))
+ Screen.blit(scaledBG,(0,0))
+    
+  
+#Function for drawing the health bars
+def Health_Bar(Health, x, y):  
+  pygame.draw.rect (Screen, Yellow, (x,y, 400,30))
+    
+#Creates both Characters
+Ronin = Fighter(200,360)
+Samurai = Fighter(700,360)
+
+#gameloop
+run = True
+while run:
+    
+ clock.tick(FPS)
+    
+ #Display Background
+ display_Bg()
+    
+ #Show Health Bars
+ Health_Bar(Ronin.Health, 20,20)
+ Health_Bar(Samurai.Health, 580,20)
+    
+ #Move the fighters
+ Ronin.Move(Screen_Width, Screen_Height,Screen, Samurai)
+ ##################Samurai.Move()
+    
+ #Draw Fighters
+ Ronin.Draw(Screen)
+ Samurai.Draw(Screen)
+ #event handler
+ for event in pygame.event.get():
+     if event.type == pygame.QUIT:
+         run = False
+            
+ #Updates display for background
+ pygame.display.update()
+
+
+#exit pygame
+pygame.quit()
+
---
+++ fighter.py
@@ -0,0 +1,90 @@
+import pygame
+
+class Fighter():
+ def __init__(self, x, y):
+     self.rect = pygame.Rect((x,y,100,200))
+     #controls how fast you go up and down(for character jump)
+     self.vel_y = 0
+     #makes it so that you can jump a certain distance up
+     self.jump = False
+     #can attack once per key press
+     self.attacking = False 
+     #defing attack variable
+     self.attack_type = 0
+     #Defines health
+     self.Health = 100
+           
+        
+ #Adding Movement To Characters (rectangles), Dx/Dy records the change in those coordinates
+ def Move(self, screen_width,screen_height,surface, target):
+     SPEED = 10
+     Gravity = 2
+     dx = 0
+     dy = 0
+     
+     #get Keypresses that allows keys to move the player
+     key = pygame.key.get_pressed()
+     
+      #Cant preform other movements while attacking        
+     if self.attacking == False:
+       pass
+       #Directional movement
+     if key[pygame.K_a]:#Left
+       dx = -SPEED
+     if key[pygame.K_d]:#Right
+        dx = SPEED
+      #Jumping   , and statement makes it so that the player can only jump again when on the ground/ no double jump
+     if key[pygame.K_w] and self.jump == False:
+         self.vel_y = -30
+         self.jump = True
+      #Attack
+     if key[pygame.K_r] or key[pygame.K_t]:
+         self.attack(surface, target)
+       #Determines whether to use attack 1 or 2
+         if key[pygame.K_r]:
+           self.attack_type = 1
+         if key[pygame.K_t]:
+           self.attack_type = 2
+
+
+
+
+   #adds Gravity
+     self.vel_y += Gravity
+     dy += self.vel_y
+        
+        
+    
+      
+      
+      
+     #Ensure player doesnt go off screen
+     if self.rect.left + dx < 0:
+       dx = -self.rect.left
+     if self.rect.right + dx > screen_width:
+       dx = screen_width - self.rect.right
+     if self.rect.bottom + dy > screen_height - 40:
+       self.vel_y = 0
+       self.jump = False
+       dy = screen_height - 40 - self.rect.bottom
+         
+         
+         
+     #Update Player Position
+     self.rect.x += dx
+     self.rect.y += dy
+                            #Key Note: Surface is what makes the rectangles show up
+  #Define attack variable       attack starts from the center of player rectangle, width is multiplied by 2 and overlaps half the rectangle
+ def attack(self,surface,target):
+    self.attacking = True
+    attacking_range = pygame.Rect(self.rect.centerx, self.rect.y, 2*self.rect.width, self.rect.height)
+    pygame.draw.rect(surface,(0,255,0), attacking_range)
+  #basically registers player hitting player
+    if attacking_range.colliderect(target.rect):
+      target.Health -= 10
+      print("hit")
+     
+  #Visually adds the rectangles(soon to be sprite images)
+ def Draw(self, surface):
+   pygame.draw.rect(surface, (255,0,0), self.rect)
+    
